a5215f19c0606ed11ebb3299fb8a4d6a1d27f4d0
David Chouinard <david@davidchouinard.com>
2015-09-06 01:56:33 +0000

    First pass at webpage

--- server.py
+++ server.py
@@ -1,22 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import bottle
 
 import subprocess
 
 import re
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+bottle.TEMPLATE_PATH[:] = ['templates']
+
 @bottle.route('/')
+@bottle.jinja2_view('register.html')
 def index():
-    ip = bottle.request.get('REMOTE_ADDR')
-    print ip
+    mac = get_mac_address(bottle.request.get('REMOTE_ADDR'))
+
+    if mac is None:
+        return {'error': "Error: Can't detect your MAC address from this page. <br>I`m sorry: this is a beta product, message david@davidchouinard.com for help.", 'hide_form': True}
+    else:
+        return {'mac': mac}
+
+@bottle.post('/')
+@bottle.jinja2_view('register.html')
+def register_device():
+    mac = get_mac_address(bottle.request.get('REMOTE_ADDR'))
+
+    if mac is None:
+        return {'error': "Error: Can't detect your MAC address from this page. <br>I`m sorry: this is a beta product, message david@davidchouinard.com for help."}
+
+    name = bottle.request.forms.get('name')
+    email = bottle.request.forms.get('email')
+
+    print name, email
+
+    if email is None:
+        return {'error': "Email is required"}
+
+    return {'success': "You'll get new snippets from now on 👻"}
+
+@bottle.route('/assets/<path:path>')
+def static(path):
+    return bottle.static_file(path, root='assets')
+
+def get_mac_address(ip):
     subprocess.call(['ping', '-c', '1', ip])
     response = subprocess.check_output(["arp", "-n", ip])
     search = re.search(r"(([a-f\d]{1,2}\:){5}[a-f\d]{1,2})", response)
 
     if search is None:
-        return "Mac address not found"
+        return None
     else:
-        mac = search.groups()[0]
-        return bottle.template("Your mac address is: {{mac}}", mac=mac)
+        return search.groups()[0]
 
 def start():
     print("* starting server")
