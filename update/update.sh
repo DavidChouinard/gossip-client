2c4c743034a1663c619e62b537cc580e65bbf5fb
David Chouinard <david@davidchouinard.com>
2015-09-19 12:53:52 -0700

    Added package update logic to update script

--- update/update.sh
+++ update/update.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 # your repository folder
 cd "/opt/recap-client/"
- 
+
 # fetch changes, git stores them in FETCH_HEAD
 git fetch
- 
+
 # check for remote changes in origin repository
 newUpdatesAvailable=`git diff HEAD FETCH_HEAD`
 if [ "$newUpdatesAvailable" != "" ]
 then
         service gossip stop
 
-        #git checkout master
         git merge FETCH_HEAD
 
         service gossip start
+
+        xargs apt-get install -y < "pkglist"
+
+        pip install -r "requirements.txt"
 
         echo "latest update installed"
 
