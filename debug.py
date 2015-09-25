58a29a13764ed5229730b5b18355a7ce65ea137d
David Chouinard <david@davidchouinard.com>
2015-09-25 09:01:29 -0400

    Added additional debug command

--- debug.py
+++ debug.py
@@ -1,11 +1,17 @@
 import networking
+
 import sys
+import tinydb
 
 from pprint import pprint
+
+db = tinydb.TinyDB('db/db.json')
 
 def main():
     if len(sys.argv) == 1 or sys.argv[1] == "proximity":
         pprint(networking.devices_in_proximity())
+    elif sys.argv[1] == "hostnames":
+        pprint(db.search(tinydb.where('hostname')))
     else:
         print("Unknown debug command")
 
